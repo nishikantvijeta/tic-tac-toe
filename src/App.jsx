dbdc9b6619a0da2838638ce7661337e25348031f
nishikantvijeta <nishikantk.ug22.ec@nitp.ac.in>
2024-06-27 16:44:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import Grid from './components/Grid/Grid'
+import Card from './components/Card/Card'
+function App() {
+  
+
+  return (
+    <>
+      <Grid numberofCards={9}/>
+    </>
+  )
+}
+
+export default App
